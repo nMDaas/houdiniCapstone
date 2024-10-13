8f01b707862bba0363a347deecd832bf30da15f7
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-10-13 00:38:11 -0400

    added some new tools, added attribwrangler node

--- tool.py
+++ tool.py
@@ -4,6 +4,12 @@ from PySide2.QtWidgets import QFileDialog
 
 global filepaths
 
+def loadVexString(filename):
+    with open(filename, 'r') as file:
+        vex_code_string = file.read()
+    return vex_code_string
+
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super(MyWidget,self).__init__()
@@ -16,7 +22,6 @@ class MyWidget(QtWidgets.QWidget):
         self.ui.apply_button.clicked.connect(apply)
         
 def selectMap():
-    print("hello!")
     initial_directory = "/Users/natashadaas/houdiniCapstone"  # Replace this with the desired initial directory
     dialog = QFileDialog()
     dialog.setOption(QFileDialog.DontUseNativeDialog, True)
@@ -46,14 +51,26 @@ def apply():
     hou.parm('/obj/terrain/terrainGrid/rows').set(150)
     hou.parm('/obj/terrain/terrainGrid/cols').set(150)
     
-    #create attribute from parameter node
+    # create attribute from parameter node
     n_attribFromMap = n_terrain.createNode('attribfrommap', 'attribfrommap')
     global filepaths
     hou.parm('/obj/terrain/attribfrommap/filename').set(filepaths[0])
     hou.parm('/obj/terrain/attribfrommap/uv_invertv').set(1)
     n_attribFromMap.setInput(0, n_terrainGrid)
+    
+    # create attribute wrangle node
+    n_attrib_wrangle = n_terrain.createNode('attribwrangle', 'attribwrangle')
+    for p in n_attrib_wrangle.parms():
+        print(p)
+        print(p.eval())
+    hou.parm('/obj/terrain/attribwrangle/class').set(1)
+    terrainAttribWrangleVEXpression = loadVexString('/Users/natashadaas/houdiniCapstone/terrainAttribWrangleVEXpression.txt')
+    hou.parm('/obj/terrain/attribwrangle/snippet').set(terrainAttribWrangleVEXpression)
+    #for n in n_attrib_wrangle.parms():
+        #print(n)
      
     hou.node('/obj/terrain/attribfrommap').setDisplayFlag(True)
+     
     
 win = MyWidget()
 win.show()
