60fafd61f5b01a7793db263d45c8cdd1b396ed88
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-10-14 12:37:03 -0400

    nodes added up to polyextrude, also printParms() impemented

--- tool.py
+++ tool.py
@@ -4,6 +4,11 @@ from PySide2.QtWidgets import QFileDialog
 
 global filepaths
 
+def printParms(node):
+    for p in node.parms():
+        print(p)
+        print(p.eval())
+
 def loadVexString(filename):
     with open(filename, 'r') as file:
         vex_code_string = file.read()
@@ -50,26 +55,48 @@ def apply():
     hou.parm('/obj/terrain/terrainGrid/sizey').set(500)
     hou.parm('/obj/terrain/terrainGrid/rows').set(150)
     hou.parm('/obj/terrain/terrainGrid/cols').set(150)
+    n_terrainGrid.setPosition(hou.Vector2(0, 0))  
     
     # create attribute from parameter node
     n_attribFromMap = n_terrain.createNode('attribfrommap', 'attribfrommap')
     global filepaths
-    hou.parm('/obj/terrain/attribfrommap/filename').set(filepaths[0])
+    deleteThisPLEASE = "/Users/natashadaas/houdiniCapstone/inputImages/imageNoLand.jpg"
+    hou.parm('/obj/terrain/attribfrommap/filename').set(deleteThisPLEASE)
     hou.parm('/obj/terrain/attribfrommap/uv_invertv').set(1)
+    n_attribFromMap.setPosition(hou.Vector2(2, 0)) 
     n_attribFromMap.setInput(0, n_terrainGrid)
+    
+    # create attribute promote node
+    n_attrib_promote = n_terrain.createNode("attribpromote", "attribpromote")
+    hou.parm('/obj/terrain/attribpromote/inname').set("Cd")
+    hou.parm('/obj/terrain/attribpromote/outclass').set(1)
+    hou.parm('/obj/terrain/attribpromote/deletein').set(0)
+    n_attrib_promote.setPosition(hou.Vector2(4, 0)) 
+    n_attrib_promote.setInput(0, n_attribFromMap)
+    
     
     # create attribute wrangle node
     n_attrib_wrangle = n_terrain.createNode('attribwrangle', 'attribwrangle')
-    for p in n_attrib_wrangle.parms():
-        print(p)
-        print(p.eval())
     hou.parm('/obj/terrain/attribwrangle/class').set(1)
     terrainAttribWrangleVEXpression = loadVexString('/Users/natashadaas/houdiniCapstone/terrainAttribWrangleVEXpression.txt')
     hou.parm('/obj/terrain/attribwrangle/snippet').set(terrainAttribWrangleVEXpression)
-    #for n in n_attrib_wrangle.parms():
-        #print(n)
-     
-    hou.node('/obj/terrain/attribfrommap').setDisplayFlag(True)
+    n_attrib_promote.setPosition(hou.Vector2(6, 0)) 
+    n_attrib_wrangle.setInput(0, n_attrib_promote)
+
+    # create polyextrude node
+    n_polyextrude_terrain = n_terrain.createNode("polyextrude", "polyextrude")
+    hou.parm('/obj/terrain/polyextrude/splittype').set(0)
+    hou.parm('/obj/terrain/polyextrude/dist').set(1.0)
+    hou.parm('/obj/terrain/polyextrude/outputback').set(1)
+    hou.parm('/obj/terrain/polyextrude/uselocalzscaleattrib').set(1)
+    hou.parm('/obj/terrain/polyextrude/localzscaleattrib').set("zextrusion")
+    n_polyextrude_terrain.setPosition(hou.Vector2(8, 0)) 
+    n_polyextrude_terrain.setInput(0, n_attrib_wrangle)
+    printParms(n_polyextrude_terrain)
+    
+    hou.node('/obj/terrain/attribwrangle').setDisplayFlag(True)
+    
+    n_terrain.layoutChildren()
      
     
 win = MyWidget()
